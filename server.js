071aff01a436c8f9a4406868772edba863ec9213
denistsoi <denistsoi@gmail.com>
2017-05-08 00:34:23 +0800

    ADD: [server] placeholder routes for search endpoint

--- server.js
+++ server.js
@@ -11,7 +11,25 @@ var app = express();
 mongoose.connect(process.env.MONGODB_URL);
 var db = mongoose.connection;
 
+/**
+ * connect to db, 
+ */
 db.on('error', console.error.bind(console, 'connection error: '));
 db.once('open', ()=> {
 
+});
+
+app.use(express.static(`${__dirname}/public`));
+
+app.post('/search', (req, res) => {
+  var query = req.query;
+  // go fetch movie locations based on query
+  // could be movie name, fuzzy search, actor , director or location. 
+
+  // send json response
+    res.json()
+});
+
+app.listen(port, ()=>{
+  debug('server is listening to port: ', port);
 });
